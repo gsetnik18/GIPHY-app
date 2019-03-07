bdfd94481e270c03890ec4da136df116d0b6e5a7
Gregory Setnik <gsetnik18@gmail.com>
2019-03-07 18:26:54 -0500

    broken right now, but working on it

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,64 +1,81 @@
 $(document).ready(function () {
 
 
-    var games = ["Skyrim" , "Devil May Cry" , "Halo" , "Red Dead Redemption"];
-    var newGame = '';
-
-    var game = $(this).data("search");
-    console.log(game);
-    
-    var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=m0lBamz8PXqygikoABJKEF4YjRhGMJ2W&q=' + game + '&limit=10&offset=0&rating=PG&lang=en';
-    console.log(queryURL);
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-        })
-
-        .then(function(response) {
-            console.log(response);
-            var results = response.data;
-
-            for (var i = 0; i<results.length; i++) {
-
-                if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-                    var gifDiv = $("<div>");
-                    gifDiv.addClass("game-gifs");
-                };
-            };
-        });
-
-    //function to search for gifs - not done yet
-    var findGifs = function () {
-        console.log("Hello World!");
-    };
-
-    $("#gif-search-go").on("click", function () {
-        findGifs();
-        addButton();
-    })
-
-    //function to create buttons from whatever is entered into search bar
-    var addButton = function () {
-        newGame = $("#gif-search-bar").val().trim();
-        games.push(newGame);
-        console.log(games);
-        displayButtons();
-    };
-
+    var games = ["Skyrim", "Devil May Cry", "Halo", "Red Dead Redemption"];
     //function to add physical buttons to HTML once they have been created in search bar
     function displayButtons() {
         $("#custom-buttons").empty();
         for (var i = 0; i < games.length; i++) {
-          var gameButton = $('<button class="btn btn-primary">');
-          gameButton.attr("id", "game");
-          gameButton.attr("data-search", games[i]);
-          gameButton.text(games[i]);
-          $("#custom-buttons").append(gameButton);
+            var gameButton = $('<button class="game">');
+            //gameButton.attr("id", "game");
+            gameButton.attr("data-search", games[i]);
+            gameButton.text(games[i]);
+            $("#custom-buttons").append(gameButton);
+
+        };
+    };
+
+
+        var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=m0lBamz8PXqygikoABJKEF4YjRhGMJ2W&q=' + "" + '&limit=10&offset=0&rating=PG&lang=en';
+        console.log(queryURL);
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+
+            .then(function (response) {
+                console.log(response);
+                var results = response.data;
+
+                for (var i = 0; i < results.length; i++) {
+                    //function to display gifs from a button click
+                    if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+                        var gifDiv = $("<div>");
+                        gifDiv.addClass("game-gifs");
+                        var gameImage = $("<img>");
+                        gameImage.attr("src", results[i].images.fixed_width_still.url);
+                        gameImage.attr("data-still", results[i].images.fixed_width_still.url);
+                        gameImage.attr("data-animate", results[i].images.fixed_width.url);
+                        gameImage.attr("data-state", "still");
+                        gameImage.addClass("gif");
+                        gifDiv.append(gameImage);
+                        $("#gifs-go-here").append(gifDiv);
+                        games.push(game);
+                        addButton();
+                    };
+                };
+            });
+
+        $("#gif-search-go").on("click", function (event) {
+            event.preventDefault();
+            var game = $("#gif-search-bar").val().trim();
+            if (game === "") {
+                return;
+            };
+        });
+        //function to search for gifs - not done yet
+        $(".game").on("click", function(e) {
+            e.preventDefault();
+            console.log('!!')
+            console.log(e);
+        });
+
+        $("#gif-search-go").on("click", function (e) {
+            console.log(e);
+            //findGifs();
+            addButton();
+        })
+
+        //function to create buttons from whatever is entered into search bar
+        var addButton = function () {
+            newGame = $("#gif-search-bar").val().trim();
+            games.push(newGame);
+            console.log(games);
+            displayButtons();
         };
 
+    });
 
 
-};
-});
 //program needs to accept input, read input, reach out to GIPHY database, and use the API search to return relevant gifs
