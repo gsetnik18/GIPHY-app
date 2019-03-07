3160dbfb43aaf280f99130755da15805986dca0b
Gregory Setnik <gsetnik18@gmail.com>
2019-03-07 10:11:02 -0500

    removed premade buttons; added js function to create buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,16 @@
 $(document).ready(function () {
 
 
-    var subjects = [];
+    var games = ["D&D" , ""];
     var newGame = '';
 
+    var x = $(this).data("search");
+    console.log(x);
+    
+    var queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + x + 'api_key=m0lBamz8PXqygikoABJKEF4YjRhGMJ2W&limit=10&offset=0&rating=G&lang=en';
+    console.log(queryURL);
+
+    //function to search for gifs - not done yet
     var findGifs = function () {
         console.log("Hello World!");
     };
@@ -13,11 +20,25 @@ $(document).ready(function () {
         addButton();
     });
 
+    //function to create buttons from whatever is entered into search bar
     var addButton = function () {
         newGame = $("#gif-search-bar").val().trim();
-        subjects.push(newGame);
-        console.log(subjects);
+        games.push(newGame);
+        console.log(games);
+        displayButtons();
     };
 
+    //function to add physical buttons to HTML once they have been created in search bar
+    function displayButtons() {
+        $("#custom-buttons").empty();
+        for (var i = 0; i < games.length; i++) {
+          var a = $('<button class="btn btn-primary">');
+          a.attr("id", "game");
+          a.attr("data-search", games[i]);
+          a.text(games[i]);
+          $("#custom-buttons").append(a);
+        }
+
+};
 });
 //program needs to accept input, read input, reach out to GIPHY database, and use the API search to return relevant gifs
