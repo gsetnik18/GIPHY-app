ac986a96939396b3a46d0cdd9c39f977c4037e7a
Gregory Setnik <gsetnik18@gmail.com>
2019-03-27 18:36:05 -0400

    Finally creates gifs on click!

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,52 +4,50 @@
     function displayButtons() {
         $("#custom-buttons").empty();
         for (var i = 0; i < games.length; i++) {
-            var gameButton = $("<button class=“game”>");
-            $(".game").bind("click", function(e){
-                e.preventDefault();
-            console.log("!!")
-            console.log(e);
-            });
+            var gameButton = $("<button class='game'>");
             //gameButton.attr(“id”, “game”);
             gameButton.attr("data-search", games[i]);
             gameButton.text(games[i]);
             $("#custom-buttons").append(gameButton);
         };
     };
-        var game = $(this).attr("data-search");
 
-        var queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + "" + '&api_key=m0lBamz8PXqygikoABJKEF4YjRhGMJ2W&limit=10&offset=0&lang=en';
+    function displayGifs(data){
+        for (var i = 0; i < data.length; i++) {
+            //function to display gifs from a button click
+            if (data[i].rating !== "r" && data[i].rating !== "pg-13") {
+                var gifDiv = $("<div>");
+                gifDiv.addClass("game-gifs");
+                var gameImage = $("<img>");
+                gameImage.attr("src", data[i].images.fixed_width_still.url);
+                gameImage.attr("data-still", data[i].images.fixed_width_still.url);
+                gameImage.attr("data-animate", data[i].images.fixed_width.url);
+                gameImage.attr("data-state", "still");
+                gameImage.addClass("gif");
+                gifDiv.append(gameImage);
+                $("#gifs-go-here").append(gifDiv);
+            };
+        };
+    }
+
+
+
+    var getGifs = function(text){
+        var queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + text + '&api_key=m0lBamz8PXqygikoABJKEF4YjRhGMJ2W&limit=10&offset=0&lang=en';
         console.log(queryURL);
-    
-    
         $.ajax({
             url: queryURL,
             method: "GET"
-        })
-
-            .then(function (response) {
+        }).then(function (response) {
                 console.log(response);
                 var results = response.data;
-
-                for (var i = 0; i < results.length; i++) {
-                    //function to display gifs from a button click
-                    if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-                        var gifDiv = $("<div>");
-                        gifDiv.addClass("game-gifs");
-                        var gameImage = $("<img>");
-                        gameImage.attr("src", results[i].images.fixed_width_still.url);
-                        gameImage.attr("data-still", results[i].images.fixed_width_still.url);
-                        gameImage.attr("data-animate", results[i].images.fixed_width.url);
-                        gameImage.attr("data-state", "still");
-                        gameImage.addClass("gif");
-                        gifDiv.append(gameImage);
-                        $("#gifs-go-here").append(gifDiv);
-                        games.push(game);
-                        addButton();
-                    };
-                };
+                displayGifs(results)
             });
-        
+    };
+
+
+
+
         $("#gif-search-go").on("click", function (event) {
             event.preventDefault();
             var game = $("#gif-search-bar").val().trim();
@@ -61,10 +59,12 @@
             displayButtons();
         });
         //function to search for gifs - not done yet
-        $(".game").on("click", function(e) {
-            e.preventDefault();
-            console.log('!!')
-            console.log(e);
+        $("#custom-buttons").on("click", ".game", function() {
+            console.log('!!');
+            console.log($(this).text()); 
+            getGifs($(this).text());
         });
+
+
     
 //program needs to accept input, read input, reach out to GIPHY database, and use the API search to return relevant gifs
